53b0e335b022210aaa9d1e7a0eba4047b05f61f1
Eyimofe Omotayo <eyimofeomotayo@gmail.com>
2022-07-04 23:49:11 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express()
+const bcrypt = require('bcrypt')
+
+app.use(express.json())
+
+const users = []
+
+app.get('/users',(req, res) => {
+    res.json(users)
+})
+ app.post('/users', async (req,res) => {
+    let {name,password} = req.body
+    try {
+        // const salt = await bcrypt.genSalt()
+        const hashedPassword = await bcrypt.hash(password, 10)
+        // console.log(salt,'🧂');
+        console.log(hashedPassword,'🔐')
+        const user = {name, password:hashedPassword}
+        users.push(user)
+        res.status(201).send()
+    }catch {
+        res.status(500).send()
+    }
+ })
+ app.post('/users/login',async (req,res) => {
+    let {name,password} = req.body
+    const user = users.find(user => user.name = name)
+    if(user == null){
+       return res.status(400).send('Cannot find user')
+    }
+    try {
+   if(await bcrypt.compare(password, user.password)) {
+    res.send('Success')
+   }else {
+    res.send('Login authentication failed try again')
+   }
+    }catch {
+        res.send('failed')
+    }
+ })
+
+
+app.listen(3000, () => {
+    console.log('Server up at Port: 3000 ✅');
+})
